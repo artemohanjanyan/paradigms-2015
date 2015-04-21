86bb0a85378b2067cb6ea9c18329bf568042da27
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2015-04-21 17:30:32 +0300

    Output format unified

--- javascript/RunJS.java
+++ javascript/RunJS.java
@@ -4,38 +4,29 @@ import java.io.*;
 public class RunJS {
     public static class IO {
         private final ScriptEngine engine;
-        public IO(ScriptEngine engine) {
+        public IO(final ScriptEngine engine) {
             this.engine = engine;
         }
 
-        public void print(String[] messages) {
-            for (int i = 0; i < messages.length; i++) {
-                if (messages[i] != null) {
-                    if (i > 0) {
-                        System.out.print(" ");
-                    }
-                    System.out.print(messages[i]);
-                }
-            }
+        public void print(final String message) {
+            System.out.print(message);
         }
 
-        public void println(String[] messages) {
-            print(messages);
-            System.out.println();
+        public void println(final String message) {
+            System.out.println(message);
         }
 
-        public void include(String file) throws Exception {
+        public void include(final String file) throws Exception {
             engine.eval(new InputStreamReader(new FileInputStream(file), "UTF-8"));
         }
     }
-    public static void main(String[] args) throws ScriptException, IOException {
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
+    public static void main(final String[] args) throws ScriptException, IOException {
+        final ScriptEngineManager factory = new ScriptEngineManager();
+        final ScriptEngine engine = factory.getEngineByName("JavaScript");
         engine.put("io", new IO(engine));
         engine.eval(
-            "var toArray = function(args){ return Array.prototype.map.call(args, function (a) { return '' + a})}; "+
-            "var println = function() { io.println(toArray(arguments)); };" + 
-            "var print   = function() { io.print  (toArray(arguments)); };" +
+            "var println = function() { io.println(Array.prototype.map.call(arguments, String).join(' ')); };" +
+            "var print   = function() { io.print  (Array.prototype.map.call(arguments, String).join(' ')); };" +
             "var include = function(file) { io.include(file); }"
         );
 
